b51d52756a9f4f9826c7ad8095bf5f53ec446200
- <->
2019-07-30 19:18:27 +0800

    master

--- coreos/caddy.sh
+++ coreos/caddy.sh
@@ -4,8 +4,9 @@ curl https://getcaddy.com | sudo bash -s personal tls.dns.dnspod,http.expires,ht
 # 为了给予caddy从非root用户（用sudo模式启动）绑定80、443等端口的权限，需要进行下面的配置，这个配置很重要，不执行这句话会导致caddy服务启动失败。
 setcap 'cap_net_bind_service=+ep' /usr/local/bin/caddy
 adduser --home /var/www --shell /sbin/nologin --system --group www
-mkdir -p /mnt/data/caddy/ssl
-chown www:www /mnt/data/caddy/ssl
+mkdir -p /mnt/caddy/ssl
+touch /mnt/caddy/Caddyfile
+chown -R www:www /mnt/caddy
 mkdir -p /var/www
 chown www:www /var/www
 fi
