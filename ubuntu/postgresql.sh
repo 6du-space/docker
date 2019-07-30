6b31f2b3f314e71e094390c1abbae18da8cb6310
- <->
2019-07-30 22:32:09 +0800

    master

--- ubuntu/postgresql.sh
+++ ubuntu/postgresql.sh
@@ -15,10 +15,13 @@ PASSWORD=`openssl rand -base64 128|tr -dc A-Za-z0-9|tail -c20`
 
 DB_HOST=127.0.0.1
 
-mkdir -p ~/git
-cd ~/git
-git clone git://git.postgresql.org/git/postgresql.git --depth=1
-cd pgadmin4/web
+sudo apt-get install wget ca-certificates -y
+chmod 777 /tmp
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+rm /etc/apt/sources.list.d/pgdg.list
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+sudo apt-get update
+sudo apt install pgadmin4 -y
 
 sudo -u postgres psql -U postgres -d postgres -c "alter user $DB_USER with password '$PASSWORD';"
 
