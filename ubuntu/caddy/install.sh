cb58f9f404312d8602936ea5871b096121b8e3aa
- <->
2019-07-31 19:46:57 +0800

    master

--- ubuntu/caddy/install.sh
+++ ubuntu/caddy/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+_dirname=$(realpath $(cd "$(dirname "$0")"; pwd))
+
 if ! hash caddy 2>/dev/null; then
 curl https://getcaddy.com | sudo bash -s personal tls.dns.dnspod,http.expires,http.cache
 # 为了给予caddy从非root用户（用sudo模式启动）绑定80、443等端口的权限，需要进行下面的配置，这个配置很重要，不执行这句话会导致caddy服务启动失败。
@@ -14,3 +16,7 @@ chown -R www:www /home/caddy
 mkdir -p /var/www
 chown www:www /var/www
 fi
+
+rm -rf /etc/logrotate.d/caddy
+cd /etc/logrotate.d
+ln -s $_dirname/logrotate.d/caddy .
