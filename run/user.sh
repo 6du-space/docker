79a6604ece29f72d40666d5c70340096c1619055
Your Name <you@example.com>
2019-08-05 14:15:22 +0800

    init

--- run/user.sh
+++ run/user.sh
@@ -17,6 +17,8 @@ IMAGE=daocloud.io/6dus/6du-dev
 #IMAGE=dev:latest
 NAME=$HOSTNAME
 
+DOCKER_ROOT=/mnt/docker/$NAME
+
 sudo docker network create --subnet=$SUBNET.0/16 $NET
 sudo docker rm $NAME
 sudo docker run \
@@ -25,8 +27,10 @@ sudo docker run \
 -p $PORT_RANGE:$PORT_RANGE \
 -p $SSH_PORT:22 \
 -v /var/log/docker/$NAME:/var/log \
--v /mnt/docker/$NAME/home:/home \
--v /mnt/docker/$NAME/root:/root \
+-v $DOCKER_ROOT/etc/supervisor/conf.d:/etc/supervisor/conf.d \
+-v $DOCKER_ROOT/etc/caddy:/etc/caddy \
+-v $DOCKER_ROOT/home:/home \
+-v $DOCKER_ROOT/root:/root \
 -v /tmp/docker/$NAME:/tmp \
 -v /mnt/share:/mnt/share \
 -h $HOSTNAME \
@@ -36,3 +40,8 @@ sudo docker run \
 --device /dev/fuse \
 --cap-add SYS_ADMIN \
 --restart=always $IMAGE
+
+mkdir -p $DOCKER_ROOT/root/.ssh
+chmod 700 $DOCKER_ROOT/root/.ssh
+cp ~/.ssh/authorized_keys $DOCKER_ROOT/root/.ssh
+
